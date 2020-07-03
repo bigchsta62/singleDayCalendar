4b39cd9cb66d2b954a88ba75bf310da77eb5ab68
bigchsta62 <bigchsta62@msn.com>
2020-07-03 10:58:52 -0700

    added moment.js, moved files, added some basic styling

---
+++ script.js
@@ -0,0 +1,7 @@
+//use moment JS to display todays date
+const format = 'LLLL';
+const result = moment().format(format);
+console.log(result);
+
+//append date to currentDay
+const currentDay = $('#currentDay').append(result);
